47071ade3b1b0fc3089b70055592d6afb7f70810
Ronda Bergman <rondabergman@hotmail.com>
2016-12-20 13:14:18 -0500

    Initial Commit

---
+++ BabysitterKata/BabysitterKataTests/ProgramTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BabysitterKata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BabysitterKata.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+    }
+}
